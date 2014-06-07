eaa7e40b5be69787bbc3e2a8856816d5d6d6f3d1
Ibex-2 <capra@clubcapra.com>
2014-06-07 16:46:06 -0400

    Correct GPS position according to IMU datum

--- src/enu_rotator/enu_rotator.py
+++ src/enu_rotator/enu_rotator.py
@@ -12,49 +12,46 @@ from sensor_msgs.msg import Imu
 
 class EnuRotator:
     def __init__(self):
-        self.publisher = rospy.Publisher('/vo', Odometry)
+        self.publisher = rospy.Publisher('/gps', Odometry)
         self.subscriber = rospy.Subscriber('/gps/enu', Odometry, self.enu_callback)
         self.subscriber = rospy.Subscriber('/imu_datum', Imu, self.imu_callback)
         self.rotated_enu = None
         self.rotation = None
-        self.has_new_data = False
-        self.x = 0
-        self.y = 0
-        self.theta = 0
-        self.has_theta = False
+
         rospy.init_node('enu_rotator')
 
     def publish_loop(self):
         while not rospy.is_shutdown():
-            if self.has_theta and self.has_new_data:
+            if self.rotated_enu is not None:
                 odom = Odometry()
 
                 odom.header.frame_id = "odom"
                 odom.header.stamp = rospy.get_rostime()
-                odom.pose.pose.position.y = self.y#-self.rotated_enu.tolist()[0][0]
-                odom.pose.pose.position.x = self.x#-self.rotated_enu.tolist()[0][1]
+                odom.pose.pose.position.y = self.rotated_enu.tolist()[0][0]
+                odom.pose.pose.position.x = -self.rotated_enu.tolist()[0][1]
+                odom.pose.covariance = self.covariance
                 odom.pose.pose.orientation.w = 1
                 odom.child_frame_id = "base_link"
-                self.has_new_data = False
                 self.publisher.publish(odom)
+                self.rotated_enu = None
             else:
                 time.sleep(1.0 / 1000)
 
     def enu_callback(self, msg):
-        if self.has_theta:
-            x = msg.pose.pose.position.x
-            y = msg.pose.pose.position.y
-            d = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
-            self.y = math.cos(self.theta) * d
-            self.x = -math.sin(self.theta) * d
-            self.has_new_data = True
+        if self.rotation is not None:
+            self.rotated_enu = numpy.matrix([msg.pose.pose.position.x, msg.pose.pose.position.y]) * self.rotation
+            self.covariance = msg.pose.covariance
 
     def imu_callback(self, msg):
         o = msg.orientation
         r, p, y = tf.transformations.euler_from_quaternion([o.x, o.y, o.z, o.w])
-        self.theta = -(y + math.pi)
-        self.has_theta = True
-#       
+        theta = y
+
+        if(theta > 0):
+            self.rotation = numpy.matrix([[math.cos(theta), -math.sin(theta)], [math.sin(theta), math.cos(theta)]])
+        else:
+            self.rotation = numpy.matrix([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])
+
 
 if __name__ == '__main__':
     er = EnuRotator()
